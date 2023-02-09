14ecf11d5b61aa8479f2fd80ea708f7e9c4a145f
Julian Giudice <54405665+karpo27@users.noreply.github.com>
2023-02-09 17:52:28 -0300

    Create AnnalynsInfiltration.java

---
+++ 3- Booleans/1- Annalyn's Infiltration/AnnalynsInfiltration.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+
+class AnnalynsInfiltration {
+    public static boolean canFastAttack(boolean knightIsAwake) {
+        return !knightIsAwake;
+    }
+
+    public static boolean canSpy(boolean knightIsAwake, boolean archerIsAwake, boolean prisonerIsAwake) {
+        ArrayList<Boolean> state = new ArrayList<Boolean>();
+        state.add(knightIsAwake);
+        state.add(archerIsAwake);
+        state.add(prisonerIsAwake);
+
+        int counter = 0;
+        for (boolean i : state){
+            if (i){
+                counter += 1;
+            }
+        }
+
+        return counter >= 1;
+    }
+
+    public static boolean canSignalPrisoner(boolean archerIsAwake, boolean prisonerIsAwake) {
+        return !archerIsAwake && prisonerIsAwake;
+    }
+
+    public static boolean canFreePrisoner(boolean knightIsAwake, boolean archerIsAwake, boolean prisonerIsAwake, boolean petDogIsPresent) {
+        if (petDogIsPresent){
+            return !archerIsAwake;
+        } else {
+            return !knightIsAwake && !archerIsAwake && prisonerIsAwake;
+        }
+    }
+}
