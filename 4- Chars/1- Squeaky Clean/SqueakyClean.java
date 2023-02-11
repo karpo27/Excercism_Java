de672f36839030ca2819beefdac3918ce87b4ead
Julian Giudice <54405665+karpo27@users.noreply.github.com>
2023-02-11 10:51:09 -0300

    Create SqueakyClean.java

---
+++ 4- Chars/1- Squeaky Clean/SqueakyClean.java
@@ -0,0 +1,19 @@
+class SqueakyClean {
+ 
+    static String clean(String identifier) {
+        String output;
+        output = identifier.replace(' ', '_'); //Task 1
+        output = output.replaceAll("[\\x00-\\x1F\\x7F]", "CTRL"); //Task 2
+        //Beginn of Task 3
+        StringBuilder stringB = new StringBuilder(output);
+        for (int i = 0; i < stringB.length(); i++) {
+            if (stringB.charAt(i) == '-'){
+                i = i+1;
+                char newChar = Character.toUpperCase(stringB.charAt(i));
+                stringB.setCharAt(i, newChar);
+                stringB.deleteCharAt(i-1);
+            }
+        }
+        return stringB.toString().replaceAll("[0-9]|[\\x{0001f300}-\\x{0001f64f}]|[\\u03B1-\\u03C9]", ""); //Task 4
+    }
+}
