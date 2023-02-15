0769bc2b82d45f7fcd5966772ec3f1f84439721b
Julian Giudice <54405665+karpo27@users.noreply.github.com>
2023-02-15 14:41:10 -0300

    Update Gigasecond.java

--- 6- Numbers/2- Gigasecond/Gigasecond.java
+++ 6- Numbers/2- Gigasecond/Gigasecond.java
@@ -4,17 +4,17 @@ import java.time.Month;
 
 public class Gigasecond {
     long gigasec = 1000000000;
-    private LocalDateTime dateTime;
+    private LocalDateTime moment;
 
     public Gigasecond(LocalDate moment) {
         this(moment.atTime(0, 0, 0));
     }
 
     public Gigasecond(LocalDateTime moment) {
-        this.dateTime = moment.plusSeconds(gigasec);
+        this.moment = moment.plusSeconds(gigasec);
     }
 
     public LocalDateTime getDateTime() {
-        return this.dateTime;
+        return this.moment;
     }
 }
